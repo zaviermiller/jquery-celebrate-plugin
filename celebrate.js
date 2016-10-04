450e09a1cec94f23359a12a5f0b1d3916faf07f7
Jackson Miller <jackson.h.miller@gmail.com>
2016-10-04 18:54:38 -0500

    Adding a demo and some settings
    - Using a div instead of a p since there is no semantic intent.
    - Add settings for font size (min and max)
    - Add settings for rotation at end (min and max)
    - Removed the margins
    - make the movement relative to the scale of the growth

--- celebrate.js
+++ celebrate.js
@@ -1,49 +1,61 @@
+(function ( $ ) {
 
-	$(document).ready(function(){
-		(function($){
-			$.fn.celebrate = function(options){
-				var settings = $.extend({
-	            		particles: 5,
-	            		radius: 200,
-	            		color: "black",
-	            		unicode: null,
-	            		complete: function() {}
-	        		},options);
-
-				return this.each(function() {
-					for(i=0;i<settings.particles;i++){
-						var particle = $("<p>"+ settings.unicode +"</p>").addClass("particle");
-						$(particle).css("position","absolute");
-						$(particle).css("margin-top",(Math.floor(Math.random())+10));
-						$(particle).css("margin-left",(Math.floor(Math.random())+10));
-						$(particle).css("font-size", "15px");
-						$(particle).css("opacity", "1");
-						$(particle).css("color", settings.color);
-						$(particle).css("transform",'rotate(' + (Math.floor(Math.random()*1000)) + 'deg)');
-						$(this).append($(particle));
-						$(particle).offset({
-							top: ($(this).offset.top + $(this).height() / 2) - ($(particle).height() / 2),
-              left: ($(this).offset.left + $(this).width() / 2) - ($(particle).width() / 2)
-						});
-						$(particle).animate(
-							{
-								"margin-top": (Math.floor(Math.random()*settings.radius)-settings.radius/2)+"px",
-								"margin-left": (Math.floor(Math.random()*settings.radius)-settings.radius/2)+"px",
-								"opacity":0,
-								"font-size": (Math.floor(Math.random())+(Math.random()*50)+35)+"px",
-								"css": ("transform",'rotate(' + (Math.floor(Math.random()*70+90)) + 'deg)')
-							},
-							{
-								"duration": Math.floor(Math.random()*700)+150,
-								"complete": function (){
-									$(this).remove();
-								}
-							}
-						);
+  $.fn.celebrate = function(options){
+      var end_size;
+      var scale;
+      var particle;
+      var settings = $.extend({
+                particles: 7,
+                radius: 75,
+                color: "black",
+                unicode: '\u2605',
+                start_size: '15',
+                min_end_size: '20',
+                max_end_size: '75',
+                max_duration: 500,
+                min_duration: 400,
+                complete: function() {}
+            },options);
+    return this.each(function() {
+      for(i=0;i<settings.particles;i++){
+				scale = Math.random();
+        end_size = (scale * (settings.max_end_size - settings.min_end_size) + settings.min_end_size);
+        particle = $("<div>"+ settings.unicode +"</div>").addClass("particle");
+        $(particle).css("position","absolute");
+        $(particle).css("font-size", settings.start_size + "px");
+        $(particle).css("opacity", 1);
+        $(particle).css("z-index", 9999);
+        $(particle).css("color", settings.color);
+        $(particle).css("transform",'rotate(' + (Math.random() * 70) + 'deg)');
+        $(this).append($(particle));
+        $(particle).offset({
+          top: $(this).offset().top + ($(this).height() / 2),
+          left: $(this).offset().left + ($(this).width() / 2)
+        });
+        $(particle).animate(
+          {
+            "left": $(particle).offset().left + ((Math.random() < 0.5 ? -1 : 1) * (Math.random() * settings.radius)) +"px",
+						"top": $(particle).offset().top + ((Math.random() < 0.5 ? -1 : 1) * (Math.random() * settings.radius)) +"px",
+            "opacity":0.5,
+            "z-index": 9999,
+            "font-size": end_size +"px"
+          },
+					{
+						"duration": (Math.random() * (settings.max_duration - settings.min_duration) + settings.min_duration),
+						"complete": function (){
+							$(this).animate({
+                "opacity": 0
+              },{
+                "duration": settings.duration * 0.5,
+                "complete": function(){
+                  $(this).remove();
+                }
+              });
+						}
 					}
-	        			settings.complete.call(this);
-	    			});
-
-			};
-		}(jQuery));
-	});
+				);
+      }
+      settings.complete.call(this);
+    });
+  };
+}( jQuery ));
